0ddfbe32e87a96ebc6b3882e6d4f8a0c5699e6d6
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-18 10:02:47 +0930

    bing bong

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,14 +1,27 @@
-import json from "@rollup/plugin-json";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-  input: "dist/esm/index.js",
-  output: {
-    dir: "dist/esm/",
-    format: "esm",
+export default [
+  {
+    input: 'dist/esm/index.js',
+    output: {
+      dir: 'dist/esm/',
+      format: 'esm',
+    },
+    plugins: [
+      json({ compact: true }),
+      nodeResolve({ resolveOnly: [/^@jbx-protocol\/.*$/] }),
+    ],
   },
-  plugins: [
-    json({ compact: true }),
-    nodeResolve({ resolveOnly: [/^@jbx-protocol\/.*$/] }),
-  ],
-};
+  {
+    input: 'dist/cjs/index.js',
+    output: {
+      dir: 'dist/cjs/',
+      format: 'cjs',
+    },
+    plugins: [
+      json({ compact: true }),
+      nodeResolve({ resolveOnly: [/^@jbx-protocol\/.*$/] }),
+    ],
+  },
+];
